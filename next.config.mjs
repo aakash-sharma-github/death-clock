a7f106f844e5cf374c152b45d4c2acf4a7aa100e
Aakash Sharma <96511531+aakash-sharma-github@users.noreply.github.com>
2025-03-16 16:35:22 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export", // Exports as static files
+    output: "export", // Exports as static files
     distDir: "death-clock-extension", // Custom build directory
 };
 
